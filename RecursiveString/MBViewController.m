3011843fc44e4d2b75de57b8280f05d7d7e4b78e
Moshe Berman <Moshe@Lemur.local>
2012-08-09 15:06:36 -0400

    initial

--- RecursiveString/MBViewController.m
+++ RecursiveString/MBViewController.m
@@ -18,12 +18,83 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    //
+    //  Uncomment one of these four lines and then Commind+I to profile with Allocations
+    //
+    
+ 
+//    [self loopStringNoAutorelease];
+//    [self loopMutableNoAutorelease];
+//    [self loopStringWithAutorelease];
+//    [self loopMutableWithAutorelease];
+    
 }
 
-- (void)viewDidUnload
-{
-    [super viewDidUnload];
-    // Release any retained subviews of the main view.
+- (void) loopStringNoAutorelease{
+    
+    NSString *text;
+    
+    for (NSInteger i = 0; i < 600000000; i++) {
+        
+        NSString *newText = [text stringByAppendingString:@" Hello"];
+        
+        if (text) {
+            text = newText;
+        }else{
+            text = @"";
+        }
+    }
+}
+
+- (void) loopStringWithAutorelease{
+    
+    NSString *text;
+    
+    for (NSInteger i = 0; i < 600000000; i++) {
+        
+        @autoreleasepool {
+            NSString *newText = [text stringByAppendingString:@" Hello"];
+            
+            if (text) {
+                text = newText;
+            }else{
+                text = @"";
+            }
+        }
+    }
+}
+
+- (void) loopMutableNoAutorelease{
+    
+    NSMutableString *text;
+    
+    for (NSInteger i = 0; i < 600000000; i++) {
+        
+        if (text) {
+            [text appendString:@" Hello"];
+        }else{
+            text = [@"" mutableCopy];
+        }
+    }
+    
+}
+
+- (void) loopMutableWithAutorelease{
+    
+    NSMutableString *text;
+    
+    for (NSInteger i = 0; i < 600000000; i++) {
+        @autoreleasepool {
+            
+            if (text) {
+                [text appendString:@" Hello"];
+            }else{
+                text = [@"" mutableCopy];
+            }
+        }
+    }
+    
 }
 
 - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
